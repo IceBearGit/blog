dce53b9a0f8f80813223310105e09ef93352a590
IceBearGit <73390176+IceBearGit@users.noreply.github.com>
2020-12-22 16:51:04 +0200

    3 commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  #belongs_to :author
   validates :title, presence: true, length: { maximum: 80 }
   validates :content, presence: true, length: { maximum: 200 }
   validates :name, presence: true
